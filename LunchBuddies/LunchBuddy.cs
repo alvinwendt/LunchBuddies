47ddd2695dbc6f73378b1303ed93a9fb8158aa65
alvinwendt <alvinwendt@gmail.com>
2023-01-28 13:06:36 -0600

    Add project files.

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBuddies
+{
+    public class LunchBuddy
+    {
+        public string FirstName;
+        public string LastName;
+
+
+        public static void eat(string food, List<LunchBuddy> companions);
+        {
+  
+        }
+    }
+}
---
+++ LunchBuddies/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBuddies
+{
+    public class Restaurant
+    {
+        private List<string> restaurants { get; set; }
+        public string Name { get; set; }    
+    }
+}
